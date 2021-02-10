40efbf19d83fa7d58e54c09bdca32c4cd3d2bcb4
Bellun <fhanteros@gmail.com>
2021-02-10 12:14:16 -0300

    create função

--- springboot-backend/springboot-backend/src/main/java/net/java/springboot/controller/PassageiroController.java
+++ springboot-backend/springboot-backend/src/main/java/net/java/springboot/controller/PassageiroController.java
@@ -2,10 +2,11 @@ package net.java.springboot.controller;
 
 import java.util.List;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,11 +21,16 @@ public class PassageiroController {
 	@Autowired
 	private PassageiroRepository passageiroRepository;
 
-	//pega todos passageiros
+	//listar todos passageiros
 	@GetMapping("/passageiros")
 	public List<Passageiro> getAllPassageiros(){
 		return passageiroRepository.findAll();
 	}
 	
+	//criar passageiro rest api
+	@PostMapping("/passageiros")
+	public Passageiro createPassageiro(@RequestBody Passageiro passageiro) {
+		return passageiroRepository.save(passageiro);
+	}
 	
 }
