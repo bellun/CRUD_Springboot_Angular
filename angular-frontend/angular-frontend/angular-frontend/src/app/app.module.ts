f378cb0a0d50414f780e66192748ee87535eac61
Bellun <fhanteros@gmail.com>
2021-02-10 14:51:46 -0300

    update funcionou sai zica

--- angular-frontend/angular-frontend/angular-frontend/src/app/app.module.ts
+++ angular-frontend/angular-frontend/angular-frontend/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import {FormsModule} from '@angular/forms'
+import {FormsModule} from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
