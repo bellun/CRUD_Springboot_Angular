99f75df6bf3e8319abf5c01f52e07957913ba26c
Bellun <fhanteros@gmail.com>
2021-02-10 12:02:43 -0300

    Criando funcionalidade de listar e básico de CSS

--- angular-frontend/angular-frontend/angular-frontend/src/app/app-routing.module.ts
+++ angular-frontend/angular-frontend/angular-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { PassageiroListComponent } from './passageiro-list/passageiro-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'passageiros', component: PassageiroListComponent},
+  {path :'', redirectTo: 'passageiros', pathMatch: 'full'}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ springboot-backend/springboot-backend/src/main/resources/tabela.sql
@@ -0,0 +1,13 @@
+INSERT INTO passageiros
+VALUES (1, 'Casssio','Ramos','cassio.corinthians@ghmail.com');
+
+
+INSERT INTO passageiros
+VALUES (2, 'Danilo','Avelar','danilo.avelenda@gmail.com');
+
+
+INSERT INTO passageiros
+VALUES (3, 'Fagner','Mascarado','pescoco_para_baixo_e_canela@ghmail.com');
+
+INSERT INTO passageiros
+VALUES (4, 'Gil','Monstro','gilzagueiro@gmail.com');
