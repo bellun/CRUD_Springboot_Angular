d1468d04b195df3c976f59f77b811dad4acfda54
Bellun <fhanteros@gmail.com>
2021-02-11 01:03:05 -0300

    delete, update criados

--- springboot-backend/springboot-backend/src/main/java/net/java/springboot/controller/PassageiroController.java
+++ springboot-backend/springboot-backend/src/main/java/net/java/springboot/controller/PassageiroController.java
@@ -1,10 +1,13 @@
 package net.java.springboot.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -58,5 +61,17 @@ public class PassageiroController {
 		 
 		Passageiro passageiroAtualizado = passageiroRepository.save(passageiro);
 		return ResponseEntity.ok(passageiroAtualizado);
+	}
+	
+	//deletar
+	@DeleteMapping("/passageiros/{id}")
+	public ResponseEntity< Map<String, Boolean>> deletePassageiro(@PathVariable Long id ) {
+		Passageiro passageiro = passageiroRepository.findById(id)
+				.orElseThrow(()-> new ResourceNotFoundException("Passageiro não existe com o id:"+ id));
+		passageiroRepository.delete(passageiro);
+		Map<String, Boolean> response = new HashMap<>();
+		response.put("deleted", Boolean.TRUE);
+		return ResponseEntity.ok(response);
+		
 	}
 }
